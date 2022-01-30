e7d0fc7494f7dc557c7b03af164c9b4362b4b864
Marius Lundgård <studio@mariuslundgard.com>
2022-01-30 23:25:12 +0100

    refactor(ui): use common `EMPTY_RECORD` constant

--- packages/@sanity/ui/src/constants.ts
+++ packages/@sanity/ui/src/constants.ts
@@ -1,2 +1,2 @@
-export const EMPTY_ARRAY: any[] = []
-export const EMPTY_RECORD: Record<string, any> = {}
+export const EMPTY_ARRAY: never[] = []
+export const EMPTY_RECORD: Record<string, never> = {}
