40c2575c94df9daf94ebd7e13ffec08f8ac1b1a4
Marius Lundgård <studio@mariuslundgard.com>
2022-04-17 17:56:57 +0200

    fix(examples): fix type issues after upgrading `react`

--- examples/web/pages/_document.tsx
+++ examples/web/pages/_document.tsx
@@ -8,6 +8,7 @@ import NextDocument, {
 } from 'next/document'
 import React from 'react'
 import {ServerStyleSheet} from 'styled-components'
+import {FIXME} from '../types'
 
 class Document extends NextDocument<DocumentProps & {styleTags: React.ReactNode}> {
   static async getInitialProps(ctx: DocumentContext) {
@@ -32,7 +33,7 @@ class Document extends NextDocument<DocumentProps & {styleTags: React.ReactNode}
             {initialProps.styles}
             {sheet.getStyleElement()}
           </>
-        ),
+        ) as FIXME,
       }
     } finally {
       sheet.seal()
