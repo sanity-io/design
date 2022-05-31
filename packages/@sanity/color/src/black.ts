160aa24442b1ddd1756594c0ac5834e6171ccf30
Marius Lundgård <studio@mariuslundgard.com>
2022-05-31 22:57:47 +0200

    refactor(color): move exported members to files

---
+++ packages/@sanity/color/src/black.ts
@@ -0,0 +1,8 @@
+import {black as blackHex} from './config'
+import {ColorValue} from './types'
+
+/**
+ * The black color value.
+ * @public
+ */
+export const black: ColorValue = {title: 'Black', hex: blackHex}
