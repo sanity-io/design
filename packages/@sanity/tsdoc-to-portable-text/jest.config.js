63f0e6f1f33bf02f9fa7ed38527574f279258e92
Marius Lundgård <studio@mariuslundgard.com>
2022-05-31 23:44:54 +0200

    test(tsdoc-to-portable-text): update ignore patterns

--- packages/@sanity/tsdoc-to-portable-text/jest.config.js
+++ packages/@sanity/tsdoc-to-portable-text/jest.config.js
@@ -2,11 +2,15 @@
 
 'use strict'
 
+const path = require('path')
 const {createJestConfig} = require('../../../test/jestConfig')
 
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = createJestConfig({
   displayName: require('./package.json').name,
-  modulePathIgnorePatterns: ['<rootDir>/lib/', '<rootDir>/test/__fixtures__/'],
+  modulePathIgnorePatterns: [
+    path.resolve(__dirname, 'lib/'),
+    path.resolve(__dirname, 'test/__fixtures__/'),
+  ],
   testEnvironment: 'node',
 })
