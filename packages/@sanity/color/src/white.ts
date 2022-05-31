4502579c828f844e8d1a0fd1f819769d81c05880
Marius Lundgård <studio@mariuslundgard.com>
2022-05-31 22:57:47 +0200

    refactor(color): move exported members to files

---
+++ packages/@sanity/color/src/white.ts
@@ -0,0 +1,8 @@
+import {white as whiteHex} from './config'
+import {ColorValue} from './types'
+
+/**
+ * The white color value.
+ * @public
+ */
+export const white: ColorValue = {title: 'White', hex: whiteHex}
