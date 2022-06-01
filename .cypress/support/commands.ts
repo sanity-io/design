986296acd71aa50f5a655345b88dac3989eb5ef8
Marius Lundgård <studio@mariuslundgard.com>
2022-06-01 09:00:15 +0200

    chore: delete .cypress

--- .cypress/support/commands.ts
+++
@@ -1,5 +0,0 @@
-// See https://on.cypress.io/custom-commands
-
-Cypress.Commands.overwrite('visit', (originalFn, url) => {
-  return originalFn({...url, url: `http://localhost:9009/.workshop/frame/?path=${url}`})
-})
