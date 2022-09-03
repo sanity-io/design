c3ce91b86fdc238e768e1c0a5d63f4b590fe9d5d
Marius Lundgård <studio@mariuslundgard.com>
2022-09-03 11:15:06 +0200

    tmp

--- packages/@sanity/ui-workshop/package.config.ts
+++ packages/@sanity/ui-workshop/package.config.ts
@@ -3,8 +3,8 @@ import {defineConfig} from '@sanity/pkg-utils'
 export default defineConfig({
   extract: {
     rules: {
-      'ae-forgotten-export': 'warn',
-      'ae-missing-release-tag': 'warn',
+      'ae-forgotten-export': 'off',
+      'ae-missing-release-tag': 'off',
     },
   },
 })
